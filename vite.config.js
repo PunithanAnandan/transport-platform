e4153c55b161e0a71f9ed04729fb8d5e345ef409
PunithanAnandan <punithan.anandan@gmail.com>
2026-04-03 14:19:19 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-```javascript
+javascript
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,4 +8,3 @@ export default defineConfig({
     port: 3000
   }
 })
-```
